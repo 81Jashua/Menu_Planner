178b161cc5a82931b87be4444746ccf8f0262c5b
beckytieman45 <beckytieman@gmail.com>
2021-02-16 20:31:47 -0700

    added Becky's message

--- app/src/main/java/com/example/menuplanner/MainActivity.java
+++ app/src/main/java/com/example/menuplanner/MainActivity.java
@@ -16,5 +16,6 @@ public class MainActivity extends AppCompatActivity {
         String hello = "hello";
         String breaked = "Did I break this";
         String Jacob = "hi from Jacob";
+        //Becky's Comment
     }
 }
