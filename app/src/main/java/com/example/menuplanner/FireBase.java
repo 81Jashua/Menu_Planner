5275f9f3048fc7527b6653aea1f8c5b2031a8911
Jashua <snyderjoshua81@gmail.com>
2021-03-01 10:24:58 -0700

    Firebase adding lists

--- app/src/main/java/com/example/menuplanner/FireBase.java
+++ app/src/main/java/com/example/menuplanner/FireBase.java
@@ -5,6 +5,8 @@ import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -21,8 +23,21 @@ public class FireBase {
         ingredient.put("Price", 3.50);
         ingredient.put("Store", "Costco");
 
-        db.collection("Planner")
-                .add(ingredient)
+        Map<String, Object> ingredient1 = new HashMap<>();
+        ingredient1.put("Name", "Eggs");
+        ingredient1.put("Cold", true);
+        ingredient1.put("Location", "");
+        ingredient1.put("Price", 0.00);
+        ingredient1.put("Store", "Costco");
+
+        Map<String, Object> recipe = new HashMap<>();
+        recipe.put("Name", "French Toast");
+        recipe.put("IsSide", false);
+        recipe.put("listExample", Arrays.asList(ingredient, ingredient1));
+        //recipe.put("Ingredients", new Map[]{ingredient, ingredient1});
+
+        db.collection("Recipe")
+                .add(recipe)
                 .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
                     @Override
                     public void onSuccess(DocumentReference documentReference) {
