d9e20b2a47020041b6f37c88964434fe4efe4e91
Emery Rodziewicz <emmsi@LAPTOP-2RRKCOC6.(none)>
2021-03-24 20:34:08 -0600

    nav bar in shopping list. More code towards list view for shopping list

--- app/src/main/java/com/example/menuplanner/ShoppingList.java
+++ app/src/main/java/com/example/menuplanner/ShoppingList.java
@@ -4,8 +4,10 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -17,6 +19,7 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 import java.util.Objects;
 
@@ -28,12 +31,20 @@ public class ShoppingList extends AppCompatActivity {
 
     //FirebaseDatabase database = FirebaseDatabase.getInstance();
     //DatabaseReference ref = database.getReference("Ingredient");
-    ListView listView;
-    ArrayList<String> arrayList = new ArrayList<>();
-    ArrayAdapter<String> arrayAdapter;
+    public ListView listView;
+    private List<Ingredient> shoppingList = new ArrayList<>();
+    public ArrayAdapter<String> arrayAdapter;
 
    // FirebaseDatabase database = FirebaseDatabase.getInstance();
    // DatabaseReference ref = database.getReference("Ingredient");
+
+    public List<Ingredient> getIngredient() {return shoppingList;}
+
+    public void setShoppingList(List<Ingredient> shoppingList) {
+        this.shoppingList = shoppingList;
+        ArrayAdapter arrayAdapter = new ArrayAdapter (this, android.R.layout.simple_list_item_1, shoppingList);
+        listView.setAdapter(arrayAdapter);
+    }
 
     /***
      *
@@ -46,6 +57,25 @@ public class ShoppingList extends AppCompatActivity {
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         //changes header for activity
         this.setTitle("Shopping List");
+    }
+
+        public void displayRecipeScreen(View HomeRecipeButton) {
+            Intent recipeIntent = new Intent(this, RecipeActivity.class);
+            startActivity(recipeIntent);
+            Log.i("display recipe", "opening recipe screen");
+        }
+
+        public void displayIngredientsListScreen(View homeIngredientButton) {
+            Intent ingredientListIntent = new Intent(this, IngredientListActivity.class);
+            startActivity(ingredientListIntent);
+            Log.i("display ingredient", "opening ingredient screen");
+        }
+
+        public void displayMenuListScreen (View HomeMenuButton) {
+            Intent menuItemIntent = new Intent(this, MenuActivity.class);
+            startActivity(menuItemIntent);
+            Log.i("display menu", "opening menu screen");
+        }
 
 
         /**
@@ -82,4 +112,3 @@ public class ShoppingList extends AppCompatActivity {
         });
          */
     }
-}
