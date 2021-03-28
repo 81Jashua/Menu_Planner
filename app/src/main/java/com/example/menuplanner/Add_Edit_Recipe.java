dd74bf9dae2d130cb1f53736860d48fefd28a7f3
Emery Rodziewicz <emmsi@LAPTOP-2RRKCOC6.(none)>
2021-03-28 13:16:56 -0600

    progress on drop down in Add Edit Recipe activity. Drop down displays list of strings. Still need to apply List of Ingredients.

--- app/src/main/java/com/example/menuplanner/Add_Edit_Recipe.java
+++ app/src/main/java/com/example/menuplanner/Add_Edit_Recipe.java
@@ -2,11 +2,27 @@ package com.example.menuplanner;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.Spinner;
 
-public class Add_Edit_Recipe extends AppCompatActivity {
+import java.lang.reflect.Array;
+
+public class Add_Edit_Recipe<adapter> extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+    public ArrayAdapter adapter;
+    private Ingredient ingredient;
+
+    EditText NametextView;
+    EditText StoretextView;
+    EditText LocationTextView;
+    EditText isColdTextView;
+    EditText PriceTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,10 +30,37 @@ public class Add_Edit_Recipe extends AppCompatActivity {
         setContentView(R.layout.activity_add__edit__recipe);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         this.setTitle("Edit Recipes");
+        //get the spinner from the xml.
+        Spinner dropdown = findViewById(R.id.ingredientDropDown1);
+//create a list of items for the spinner.
+        String[] items = new String[]{"1", "2", "three"};
+//create an adapter to describe how the items are displayed, adapters are used in several places in android.
+//There are multiple variations of this, but this is the basic variant.
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, items);
+//set the spinners adapter to the previously created one.
+        dropdown.setAdapter(adapter);
+        dropdown.setOnItemSelectedListener(this);
 
     }
 
     public void OnClickCancel(View view) { this.finish(); }
 
     public void OnClickSave(View view) { this.finish(); }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        parent.getItemAtPosition(position);
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+        //no action necessary
+    }
+
+    public void displayedIngredientScreen(View editIngredientButton)
+    {
+        Intent editIngredientIntent = new Intent(this, add_edit_ingredient.class);
+        startActivity(editIngredientIntent);
+        Log.i("Display Edit Ingredient", "Opening edit ingredient Screen");
+    }
 }
